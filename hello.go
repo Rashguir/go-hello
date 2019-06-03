dc1d62a40223ad1c485dcdc5ef58552aab025656
Nicolas Sicard <rashguir@gmail.com>
2019-06-03 22:51:25 +0200

    feat(stringutil)

--- hello.go
+++ hello.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/Rashguir/stringutil"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
 
